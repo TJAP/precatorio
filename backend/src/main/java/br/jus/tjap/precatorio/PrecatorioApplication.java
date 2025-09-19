7a1f313e5a4556c7f92010dd9b80d63baa938f2e
Pedro Homobono Machado Junior <pedro.homobono@cnj.jus.br>
2025-09-19 13:31:47 -0300

    Primeira estrutura do backend

---
+++ backend/src/main/java/br/jus/tjap/precatorio/PrecatorioApplication.java
@@ -0,0 +1,13 @@
+package br.jus.tjap.precatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecatorioApplication.class, args);
+	}
+
+}
